afa1d27d93eb972808cc03c6a8673ffae7c0163c
Matheus Teixeira <mathbteixeira@gmail.com>
2022-02-25 18:03:46 -0300

    Adicionando demais endpoints

--- my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
+++ my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -3,19 +3,34 @@ package dio.web.api.controller;
 import dio.web.api.model.Usuario;
 import dio.web.api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsers() {
         return usuarioRepository.findAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario findUser(@PathVariable("username") String username) {
+        return usuarioRepository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario) {
+        usuarioRepository.save(usuario);
     }
 }
