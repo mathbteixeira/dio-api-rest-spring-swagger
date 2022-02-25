20d517a5a3620625873ea3b9b82eb2c060d8d6cb
Matheus Teixeira <mathbteixeira@gmail.com>
2022-02-25 17:51:40 -0300

    Primeiro endpoint de Usuario

---
+++ my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -0,0 +1,21 @@
+package dio.web.api.controller;
+
+import dio.web.api.model.Usuario;
+import dio.web.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/users")
+    public List<Usuario> getUsers() {
+        return usuarioRepository.findAll();
+    }
+}
