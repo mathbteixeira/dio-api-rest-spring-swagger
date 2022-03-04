67fa898e6371d3ded5d7b83b5fbce482861c8a39
Matheus Teixeira <mathbteixeira@gmail.com>
2022-03-03 22:37:37 -0300

    Create SwaggerConfig.java

---
+++ my-first-web-api/src/main/java/dio/web/api/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package dio.web.api;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    //...INSIRA OS CÓDIGOS ABAIXO
+
+}
