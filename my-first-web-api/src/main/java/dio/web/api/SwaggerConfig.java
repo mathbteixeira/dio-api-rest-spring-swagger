6997d0864fc6f2ffb40b01ff3b729969a8ff69f8
Matheus Teixeira <mathbteixeira@gmail.com>
2022-03-04 19:30:01 -0300

    Finalizando classe SwaggerConfig

--- my-first-web-api/src/main/java/dio/web/api/SwaggerConfig.java
+++ my-first-web-api/src/main/java/dio/web/api/SwaggerConfig.java
@@ -25,4 +25,33 @@ public class SwaggerConfig {
                 "voce@seusite.com.br");
     }
 
+    private ApiInfoBuilder informacoesApi() {
+        ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+
+        apiInfoBuilder.title("Title - Rest API");
+        apiInfoBuilder.description("API exemplo de uso de Springboot REST API");
+        apiInfoBuilder.version("1.0");
+        apiInfoBuilder.termsOfServiceUrl("Termo de uso: Open Source");
+        apiInfoBuilder.license("Licença - Sua Empresa");
+        apiInfoBuilder.licenseUrl("http://www.seusite.com.br");
+        apiInfoBuilder.contact(this.contato());
+
+        return apiInfoBuilder;
+    }
+
+    @Bean
+    public Docket detalheApi() {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+
+        docket
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pacote.comseus.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(this.informacoesApi().build())
+                .consumes(new HashSet<String>(Arrays.asList("application/json")))
+                .produces(new HashSet<String>(Arrays.asList("application/json")));
+
+        return docket;
+    }
 }
